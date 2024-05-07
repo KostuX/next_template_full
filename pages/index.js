bf71d1c40a3a3c5b48b4abcc03e5c1f6b055506c
KostuX <goldiskiker@gmail.com>
2024-05-07 19:11:26 +0100

    new

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import DefaultLayout from "../layouts/default";
+
+export default function Home() {
+  return (
+    <DefaultLayout>
+      <div className="justify-between h-full text-center ">
+        <div className="border  m-5  border-theme_secondary">Top Center</div>
+
+        <span className="grid grid-cols-2 gap-4  m-3 h-56 ">
+          <span className="border  border-theme_secondary">Left Panel</span>
+
+          <span className="border  border-theme_secondary">Right Panel</span>
+        </span>
+      </div>
+    </DefaultLayout>
+  );
+}
